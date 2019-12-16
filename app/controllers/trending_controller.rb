31fc116fec522bd9806bd0c5060f06d34f508143
Tejaswi Katneni <tejaswi@carmatec.in>
2019-12-16 13:18:13 +0100

    pagination added to the trending page using kaminari gem

--- app/controllers/trending_controller.rb
+++ app/controllers/trending_controller.rb
@@ -2,6 +2,7 @@ class TrendingController < ApplicationController
 
 	def index
 		response = Trending.new.daily_trending
-		@response = JSON.parse(response.body)
+		daily_trending = JSON.parse(response.body)
+		@daily_trending = Kaminari.paginate_array(daily_trending).page(params[:page]).per(10)
 	end
 end
